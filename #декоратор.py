ca00b112f99a419f5790f3e8efb904d273599d36
Margarita83 <52213505+Margarita83@users.noreply.github.com>
2019-07-24 17:21:26 +0400

    Add files via upload

---
+++ #декоратор.py
@@ -0,0 +1,19 @@
+#декоратор 
+print("Hello World") 
+def s (): 
+l = [] 
+for i in range (1000): 
+if i % 2 == 0: 
+l.append (i) 
+return l 
+l1 = s() 
+print (l1) 
+
+def si (): 
+k = [] 
+for j in range (1000): 
+if j % 5 == 0: 
+k.append (j) 
+return k 
+k1 = si() 
+print (k1)
